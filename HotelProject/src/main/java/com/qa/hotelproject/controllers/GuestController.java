42929b4454145aaf6e4cf6b3c43713421b856c89
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-13 11:00:37 +0100

    CS: All CRUD functionality coded and tested using postman. Application.Properties still not working correctly, despite application-test.properties name update.

--- HotelProject/src/main/java/com/qa/hotelproject/controllers/GuestController.java
+++ HotelProject/src/main/java/com/qa/hotelproject/controllers/GuestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +50,18 @@ public class GuestController {
 	public Guest create(@RequestBody Guest guest) {
 		this.guestlist.add(guest);									//append to temp test list
 		return this.guestlist.get(guestlist.size() - 1);			//-1 used as mock SQL id's begin at 1, Array Lists begin at 0 - for testing purposes only.
-	}	
+	}
+	
+	//PUT - UPDATE
+	@PutMapping("/update/{id}")
+	public Guest update(@PathVariable int id, @RequestBody Guest guest) {
+		//Remove old record:
+		this.guestlist.remove(id);
+		//Adding new record:
+		this.guestlist.add(id, guest);
+		//Return new record:
+		return this.guestlist.get(id);
+	}
 	
 	//POST - Delete
 	@DeleteMapping("delete/{id}")
