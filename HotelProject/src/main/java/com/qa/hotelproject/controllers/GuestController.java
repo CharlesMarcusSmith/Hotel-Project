e49b73a8f25b9cf8e841a3f6636af9449c700916
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-13 02:43:33 +0100

    CS: Post create mapping & Get readAll mapping written and tested in postman.

--- HotelProject/src/main/java/com/qa/hotelproject/controllers/GuestController.java
+++ HotelProject/src/main/java/com/qa/hotelproject/controllers/GuestController.java
@@ -1,16 +1,40 @@
 package com.qa.hotelproject.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hotelproject.entities.Guest;
 
 @RestController
 @RequestMapping("/guest")											//localhost:8080/guest
 public class GuestController {
 	
 //	Hello Test:
+//	Used for testing functionality using postman
 	@GetMapping("/hello")											//localhost:8080/guest/hello
 	public String hello() {
 		return "Hello";
 	}
+	
+	//Temporary Array for testing CRUD, given unique name 'guestlist' to prevent confusion
+	private List<Guest> guestlist = new ArrayList<>();
+	
+	//Get - Read All Functionality
+	@GetMapping("/readAll")
+	public List<Guest> readAll() {
+		return this.guestlist;
+	}
+	
+	@PostMapping("/create")
+	public Guest create(@RequestBody Guest guest) {
+		this.guestlist.add(guest);									//append to temp test list
+		return this.guestlist.get(guestlist.size() - 1);			//-1 used as mock SQL id's begin at 1, Array Lists begin at 0 - for testing purposes only.
+	}
+	
 }
