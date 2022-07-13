2df22269741dcaab808ae27ee6db4f090b0f10e0
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-13 01:48:32 +0100

    CS: Basic controller class created.

---
+++ HotelProject/src/main/java/com/qa/hotelproject/controllers/GuestController.java
@@ -0,0 +1,10 @@
+package com.qa.hotelproject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/guest")
+public class GuestController {
+	
+}
