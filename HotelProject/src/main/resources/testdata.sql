0c3757d9305dd424b744df3a36006abb35074890
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-14 19:52:54 +0100

    CS: findAll created in Test controller.

--- HotelProject/src/main/resources/testdata.sql
+++ HotelProject/src/main/resources/testdata.sql
@@ -1,1 +1,1 @@
-INSERT INTO `guest` (`first_name`, `last_name`, `email`, `room_number`) VALUES ('Charles', 'Smith', 'charles@googlemail.com', 363);
+INSERT INTO `guest` (`first_name`, `last_name`, `email`, `room_number`) VALUES ('Charles', 'Smith', 'charles@googlemail.com', 333);
--- HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
+++ HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
@@ -1,9 +1,16 @@
 package com.qa.hotelproject.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 import org.springframework.test.context.jdbc.Sql;
@@ -11,6 +18,7 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.hotelproject.entities.Guest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,12 +29,21 @@ public class GuestControllerTest {
 	private MockMvc mvc;
 	
 	@Autowired
-	private ObjectMapper mapper;
+	private ObjectMapper om;
 	
 	@Test
-	public void getAllTest() {
+	public void getAllTest() throws Exception {
+		//The result we expect
+		List<Guest> output = new ArrayList<>();
+		Guest entry = new Guest(1L, "Charles", "Smith", "charles@googlemail.com", 333);
 		
+		//Converting output to JSON
+		String outJSON = om.writeValueAsString(output);
 		
+		//Sending request
+		mvc.perform(get("guest/readAll")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(content().json(outJSON));
 	}
 	
 	
