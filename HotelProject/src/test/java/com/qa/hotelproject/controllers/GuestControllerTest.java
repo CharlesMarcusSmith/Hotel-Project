e386762d53dcd16c0f440569dd7ac4203d7295c3
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-14 18:14:42 +0100

    CS: Guest Test Controller class create.

--- HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
+++ HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
@@ -1,6 +1,23 @@
 package com.qa.hotelproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class GuestControllerTest {
-
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper om;
+	
 }
