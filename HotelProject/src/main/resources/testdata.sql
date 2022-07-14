6bf01c221240a0735bc151ec2318d6e2df576d51
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-14 19:33:33 +0100

    CS: TestData corrected.

--- HotelProject/src/main/resources/testdata.sql
+++ HotelProject/src/main/resources/testdata.sql
@@ -1,1 +1,1 @@
-INSERT INTO `guest` (`first_name`, `last_name`, `email`, `room_number`) VALUES ('`Charles', 'Smith', 'charles@googlemail.com');
+INSERT INTO `guest` (`first_name`, `last_name`, `email`, `room_number`) VALUES ('Charles', 'Smith', 'charles@googlemail.com', 363);
--- HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
+++ HotelProject/src/test/java/com/qa/hotelproject/controllers/GuestControllerTest.java
@@ -1,5 +1,6 @@
 package com.qa.hotelproject.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,8 +21,13 @@ public class GuestControllerTest {
 	private MockMvc mvc;
 	
 	@Autowired
-	private ObjectMapper om;
+	private ObjectMapper mapper;
 	
+	@Test
+	public void getAllTest() {
+		
+		
+	}
 	
 	
 }
